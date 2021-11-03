6beb83773b242c98646daad1934bd173c817f2a4
Edmundo Paulino <manyymoore@gmail.com>
2021-11-03 15:30:48 +0000

    refactor: 💩 In progress

--- src/main.rs
+++ src/main.rs
@@ -42,41 +42,51 @@ fn main() {
         ("I", 1),
     ]);
 
-    let roman = "MMMCMXCIX";
+    let roman = "MC";
     let mut decimal: u16 = 0;
-    for place in THOUSANDS {
-        match roman.strip_prefix(place) {
-            Some(roman) => {
-                decimal += table.get(place).unwrap();
-                for place in HUNDREDS {
-                    match roman.strip_prefix(place) {
-                        Some(roman) => {
-                            decimal += table.get(place).unwrap();
-                            for place in TENS {
-                                match roman.strip_prefix(place) {
-                                    Some(roman) => {
-                                        decimal += table.get(place).unwrap();
-                                        for place in UNITS {
-                                            match roman.strip_prefix(place) {
-                                                Some(_roman) => {
-                                                    decimal += table.get(place).unwrap();
-                                                }
-                                                _ => break,
-                                            }
-                                        }
-                                        break;
-                                    }
-                                    _ => break,
-                                }
-                            }
-                            break;
-                        }
-                        _ => break,
+    thousands(&mut decimal, roman, &table);
+    assert_eq!(decimal, 1_100);
+}
+
+fn thousands(decimal: &mut u16, roman: &str, table: &HashMap<&str, u16>) {
+    match is_thousands(roman) {
+        Some(_) => {
+            for place in THOUSANDS {
+                match roman.strip_prefix(place) {
+                    Some(roman) => {
+                        *decimal += table.get(place).unwrap();
+                        hundreds(decimal, roman, table);
+                        break;
                     }
+                    _ => continue,
                 }
-                break;
             }
-            _ => break,
         }
+        None => hundreds(decimal, roman, table),
     }
+}
+
+fn is_thousands(roman: &str) -> Option<usize> {
+    roman.find('M')
+}
+
+fn hundreds(decimal: &mut u16, roman: &str, table: &HashMap<&str, u16>) {
+    match is_hundreds(roman) {
+        Some(_) => {
+            for place in HUNDREDS {
+                match roman.strip_prefix(place) {
+                    Some(roman) => {
+                        *decimal += table.get(place).unwrap();
+                        break;
+                    }
+                    _ => continue,
+                }
+            }
+        }
+        None => (),
+    }
+}
+
+fn is_hundreds(roman: &str) -> Option<usize> {
+    roman.find(|c: char| c == 'C' || c == 'D')
 }
