20b33265634c0da903b1e857c8a34afeb5167cb6
Edmundo Paulino <manyymoore@gmail.com>
2021-11-03 17:52:48 +0000

    chore: 🔥 Remove comments

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,5 @@ use roman::{run, Numeral};
 fn main() {
     let numeral = Numeral::new("MVIII".to_owned()).unwrap();
     let result = run(numeral).unwrap();
-    // thousands(&mut decimal, roman, &table);
     assert_eq!(result, 1_008);
 }
