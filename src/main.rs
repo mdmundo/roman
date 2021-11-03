d7b1ac40eb71f7c2a020bb6765186ec5ecf045f2
Edmundo Paulino <manyymoore@gmail.com>
2021-11-03 14:26:25 +0000

    refactor: 💥 Change approach

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
 use std::collections::HashMap;
 
-const ROMAN: [&str; 30] = [
-    "MMM", "MM", "M", "CM", "DCCC", "DCC", "DC", "D", "CD", "CCC", "CC", "C", "XC", "LXXX", "LXX",
-    "LX", "L", "XL", "XXX", "XX", "X", "IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I",
-];
+const THOUSANDS: [&str; 3] = ["MMM", "MM", "M"];
 
-//     Thousands   Hundreds    Tens    Units
-// 1   M           C           X       I
-// 2   MM          CC          XX      II
-// 3   MMM         CCC         XXX     III
-// 4               CD          XL      IV
-// 5               D           L       V
-// 6               DC          LX      VI
-// 7               DCC         LXX     VII
-// 8               DCCC        LXXX    VIII
-// 9               CM          XC      IX
+const HUNDREDS: [&str; 9] = ["CM", "DCCC", "DCC", "DC", "D", "CD", "CCC", "CC", "C"];
+
+const TENS: [&str; 9] = ["XC", "LXXX", "LXX", "LX", "L", "XL", "XXX", "XX", "X"];
+
+const UNITS: [&str; 9] = ["IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I"];
 
 fn main() {
-    let table: HashMap<&str, u32> = HashMap::from([
+    let table: HashMap<&str, u16> = HashMap::from([
         ("MMM", 3000),
         ("MM", 2000),
         ("M", 1000),
@@ -49,25 +41,25 @@ fn main() {
         ("II", 2),
         ("I", 1),
     ]);
-    let mut roman = String::from("MMCDXXI");
-    let mut decimal: u32 = 0;
-    // let decimal = match roman {
-    //     "MMM" => 3000,
-    //     _ => 0,
-    // };
-    for place in ROMAN {
-        match roman.matches(place).next() {
-            // Add actual value later
-            // Some(val) => decimal += 1,
-            Some(val) => {
-                let disjoint_match = roman.match_indices(val).next().unwrap();
-                let offset = disjoint_match.0 + disjoint_match.1.len();
-                roman.replace_range(..offset, "");
-                decimal += table.get(val).unwrap();
+
+    let roman = "MMMCMXCIX";
+    let mut decimal: u16 = 0;
+    for place in THOUSANDS {
+        println!("---");
+        println!("place: {}", place);
+        println!(
+            "roman.strip_prefix(place).unwrap(): {}",
+            roman.strip_prefix(place).unwrap()
+        );
+        match roman.strip_prefix(place) {
+            Some(roman) => {
+                println!("roman: {}", roman);
+                decimal += table.get(place).unwrap();
+                assert_eq!(decimal, 3_000);
+                // call next for loop
+                break;
             }
-            None => continue,
+            _ => break,
         }
     }
-    assert_eq!(roman, "");
-    assert_eq!(decimal, 2421);
 }
