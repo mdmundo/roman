1732bea20dd490f6f6535caec5e508699fbca137
Edmundo Paulino <manyymoore@gmail.com>
2021-11-03 13:04:42 +0000

    refactor: 🔥 Remove template file

--- src/example.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let mut s = String::from("MMCDXXI");
-    let disjoint_match = s.match_indices("MM").next().unwrap_or((0, ""));
-    let offset = disjoint_match.0 + disjoint_match.1.len();
-
-    s.replace_range(..offset, "");
-    assert_eq!(s, "CDXXI");
-}
