8764bf752bddc760a01ae5a6c18dd498d14319d9
Edmundo Paulino <manyymoore@gmail.com>
2021-11-03 13:05:12 +0000

    refactor: 💩 Bad code, good idea

--- src/main.rs
+++ src/main.rs
@@ -5,39 +5,6 @@ const ROMAN: [&str; 30] = [
     "LX", "L", "XL", "XXX", "XX", "X", "IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I",
 ];
 
-const VALUES: HashMap<String, u32> = HashMap::from([
-    ("MMM", 3000),
-    ("MM", 2000),
-    ("M", 1000),
-    ("CM", 900),
-    ("DCCC", 800),
-    ("DCC", 700),
-    ("DC", 600),
-    ("D", 500),
-    ("CD", 400),
-    ("CCC", 300),
-    ("CC", 200),
-    ("C", 100),
-    ("XC", 90),
-    ("LXXX", 80),
-    ("LXX", 70),
-    ("LX", 60),
-    ("L", 50),
-    ("XL", 40),
-    ("XXX", 30),
-    ("XX", 20),
-    ("X", 10),
-    ("IX", 9),
-    ("VIII", 8),
-    ("VII", 7),
-    ("VI", 6),
-    ("V", 5),
-    ("IV", 4),
-    ("III", 3),
-    ("II", 2),
-    ("I", 1),
-]);
-
 //     Thousands   Hundreds    Tens    Units
 // 1   M           C           X       I
 // 2   MM          CC          XX      II
@@ -50,6 +17,38 @@ const VALUES: HashMap<String, u32> = HashMap::from([
 // 9               CM          XC      IX
 
 fn main() {
+    let table: HashMap<&str, u32> = HashMap::from([
+        ("MMM", 3000),
+        ("MM", 2000),
+        ("M", 1000),
+        ("CM", 900),
+        ("DCCC", 800),
+        ("DCC", 700),
+        ("DC", 600),
+        ("D", 500),
+        ("CD", 400),
+        ("CCC", 300),
+        ("CC", 200),
+        ("C", 100),
+        ("XC", 90),
+        ("LXXX", 80),
+        ("LXX", 70),
+        ("LX", 60),
+        ("L", 50),
+        ("XL", 40),
+        ("XXX", 30),
+        ("XX", 20),
+        ("X", 10),
+        ("IX", 9),
+        ("VIII", 8),
+        ("VII", 7),
+        ("VI", 6),
+        ("V", 5),
+        ("IV", 4),
+        ("III", 3),
+        ("II", 2),
+        ("I", 1),
+    ]);
     let mut roman = String::from("MMCDXXI");
     let mut decimal: u32 = 0;
     // let decimal = match roman {
@@ -59,13 +58,16 @@ fn main() {
     for place in ROMAN {
         match roman.matches(place).next() {
             // Add actual value later
-            Some(val) => decimal += 1,
-            // Some(val) => {
-            //     println!("{}", val);
-            //     roman = val;
-            //     decimal += 1;
-            // }
+            // Some(val) => decimal += 1,
+            Some(val) => {
+                let disjoint_match = roman.match_indices(val).next().unwrap();
+                let offset = disjoint_match.0 + disjoint_match.1.len();
+                roman.replace_range(..offset, "");
+                decimal += table.get(val).unwrap();
+            }
             None => continue,
         }
     }
+    assert_eq!(roman, "");
+    assert_eq!(decimal, 2421);
 }
